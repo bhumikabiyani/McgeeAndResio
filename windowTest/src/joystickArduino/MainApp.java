25ac71eb5c4521180c7a26365259b741122ea5e9
superrm11 <ramtech51@comcast.net>
2016-03-12 11:49:58 -0500

    A commit

--- windowTest/src/joystickArduino/MainApp.java
+++ windowTest/src/joystickArduino/MainApp.java
@@ -1,28 +1,38 @@
 package joystickArduino;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Frame;
-import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import net.java.games.input.Component;
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import javax.swing.JPopupMenu;
-import java.awt.Component;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import javax.swing.event.MenuListener;
+import javax.swing.event.MenuEvent;
+
 
 public class MainApp extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5399019670779785323L;
 	private JPanel contentPane;
-
+	private static Controller[] con;
+	private static JMenu mnDevice;
+	private static JMenuItem[] comList;
+	private static Component[][] com;
+	private static int comListNum = 0;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -32,14 +42,34 @@ public class MainApp extends JFrame {
 				try {
 					MainApp frame = new MainApp();
 					frame.setTitle("Pi Driver Station");
-//					frame.setUndecorated(true);
+					frame.setUndecorated(true);
 					frame.setVisible(true);
-//					frame.setExtendedState(frame.getExtendedState()|Frame.MAXIMIZED_BOTH);
+					frame.setExtendedState(frame.getExtendedState()|Frame.MAXIMIZED_BOTH);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
+		//Gets Controllers and components for each
+				con = ControllerEnvironment.getDefaultEnvironment().getControllers();
+				com = new Component[con.length][];
+				
+				for(int i = 0; i < con.length; i++){
+					mnDevice.add(con[i].getName());
+					com[i] = con[i].getComponents();
+					comListNum += com[i].length;
+				}
+				comList = new JMenuItem[comListNum];
+				
+				//to traverse the entire com list
+				for(int i =0 ; i < com.length; i++){
+					for(int j = 0; j < com[i].length; j++){
+						//this will access all items in the second part of the 2d array
+					}
+				}
+				
+		
+		
 	}
 
 	/**
@@ -56,6 +86,15 @@ public class MainApp extends JFrame {
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
+		JMenuItem mntmSave = new JMenuItem("Save");
+		mnFile.add(mntmSave);
+		
+		JMenuItem mntmSaveAs = new JMenuItem("Save As");
+		mnFile.add(mntmSaveAs);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open");
+		mnFile.add(mntmOpen);
+		
 		JMenuItem mntmExit = new JMenuItem("Exit");
 		mnFile.add(mntmExit);
 		mntmExit.addActionListener(new ActionListener(){
@@ -67,11 +106,15 @@ public class MainApp extends JFrame {
 			}
 			
 		});
-		JPopupMenu pumAxis = new JPopupMenu("Axis");
+		
 		
 		JMenu mnAdd = new JMenu("Add");
 		menuBar.add(mnAdd);
-		mnAdd.add(pumAxis);
+		
+		mnDevice = new JMenu("Device");
+		
+		
+		mnAdd.add(mnDevice);
 		
 		JMenu mnSetup = new JMenu("Setup");
 		menuBar.add(mnSetup);
@@ -79,22 +122,7 @@ public class MainApp extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-	}
-	private static void addPopup(Component component, final JPopupMenu popup) {
-		component.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			public void mouseReleased(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			private void showMenu(MouseEvent e) {
-				popup.show(e.getComponent(), e.getX(), e.getY());
-			}
-		});
+		
+		
 	}
 }
