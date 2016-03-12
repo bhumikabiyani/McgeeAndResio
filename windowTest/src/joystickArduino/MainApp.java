a5e1fbc81b1fb6be1f70f01e4e2ede49c735d400
superrm11 <ramtech51@comcast.net>
2016-03-12 02:12:08 -0500

    added fullscreen and undecorated border(comented out)

--- windowTest/src/joystickArduino/MainApp.java
+++ windowTest/src/joystickArduino/MainApp.java
@@ -2,15 +2,22 @@ package joystickArduino;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Frame;
+import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainApp extends JFrame {
 
@@ -24,8 +31,10 @@ public class MainApp extends JFrame {
 			public void run() {
 				try {
 					MainApp frame = new MainApp();
-					
+					frame.setTitle("Pi Driver Station");
+//					frame.setUndecorated(true);
 					frame.setVisible(true);
+//					frame.setExtendedState(frame.getExtendedState()|Frame.MAXIMIZED_BOTH);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -58,9 +67,11 @@ public class MainApp extends JFrame {
 			}
 			
 		});
+		JPopupMenu pumAxis = new JPopupMenu("Axis");
 		
 		JMenu mnAdd = new JMenu("Add");
 		menuBar.add(mnAdd);
+		mnAdd.add(pumAxis);
 		
 		JMenu mnSetup = new JMenu("Setup");
 		menuBar.add(mnSetup);
@@ -68,5 +79,22 @@ public class MainApp extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+	}
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
 	}
 }
