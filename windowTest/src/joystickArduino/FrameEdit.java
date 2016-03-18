e609dfdd46c9146659b075312aa8e0865494b779
superrm11 <ramtech51@comcast.net>
2016-03-18 10:56:34 -0400

    added features that will implement adding buttons and axis

--- windowTest/src/joystickArduino/FrameEdit.java
+++ windowTest/src/joystickArduino/FrameEdit.java
@@ -1,62 +1,40 @@
 package joystickArduino;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JRadioButtonMenuItem;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.border.EmptyBorder;
 
 import net.java.games.input.Component;
 import net.java.games.input.Controller;
-import net.java.games.input.ControllerEnvironment;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
 
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-
-
-public class MainApp extends JFrame implements Runnable{
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 5399019670779785323L;
-	private JPanel contentPane;
+public class FrameEdit extends JFrame {
 	private static Controller[] con;
 	private static JMenu mnDevice;
 	private static JMenu mnAdd;
 	private static Component[][] com;
 	private static JRadioButtonMenuItem showAllControllers;
-	
+	private JPanel contentPane;
+
 	/**
 	 * Launch the application.
 	 */
-
-	public void run(){
-		while(true){
-			if(mnDevice.getSelectedObjects() != null){
-				
-			}
-		}
-	}
-	
-	public static void addDevice(String Label){
-		
-	}
-	
-	
 	public static void main(String[] args) {
-		(new Thread(new MainApp())).start();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainApp frame = new MainApp();
+					Frame frame = new Frame();
 					frame.setTitle("Pi Driver Station");
 //					frame.setUndecorated(true);
 					frame.setVisible(true);
@@ -66,22 +44,13 @@ public class MainApp extends JFrame implements Runnable{
 				}
 			}
 		});
-		//Gets Controllers and components for each
-				con = ControllerEnvironment.getDefaultEnvironment().getControllers();
-				com = new Component[con.length][];
-				mnDevice.removeAll();
-				for (int i = 0; i < com.length; i++){
-					if(con[i].getType() == Controller.Type.GAMEPAD || con[i].getType() == Controller.Type.KEYBOARD){
-						mnDevice.add(con[i].getName());
-					}
-				}
-		
 	}
 
 	/**
 	 * Create the frame.
 	 */
-	public MainApp() {
+	
+public FrameEdit() {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -154,23 +123,23 @@ public class MainApp extends JFrame implements Runnable{
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 139, 139, 88);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		panel.setVisible(true);
+//		JPanel panel = new JPanel();
+//		panel.setBounds(0, 139, 139, 88);
+//		contentPane.add(panel);
+//		panel.setLayout(null);
+//		panel.setVisible(false);
 		
-		JComboBox<?> comboBox = new JComboBox();
-		comboBox.setBounds(0, 0, 139, 22);
-		panel.add(comboBox);
-		
-		JButton btnAddAsButton = new JButton("Add As Button");
-		btnAddAsButton.setBounds(0, 25, 127, 25);
-		panel.add(btnAddAsButton);
-		
-		JButton btnAddAsAxis = new JButton("Add As Axis");
-		btnAddAsAxis.setBounds(0, 50, 127, 25);
-		panel.add(btnAddAsAxis);
+//		JComboBox comboBox = new JComboBox();
+//		comboBox.setBounds(0, 0, 139, 22);
+//		panel.add(comboBox);
+//		
+//		JButton btnAddAsButton = new JButton("Add As Button");
+//		btnAddAsButton.setBounds(0, 25, 127, 25);
+//		panel.add(btnAddAsButton);
+//		
+//		JButton btnAddAsAxis = new JButton("Add As Axis");
+//		btnAddAsAxis.setBounds(0, 50, 127, 25);
+//		panel.add(btnAddAsAxis);
 		
 		
 	}
