7f3fcb013409caf6098b3deb6806415c4eda109d
superrm11 <ramtech51@comcast.net>
2016-03-22 12:39:13 -0400

    added simple TestForHardware Class for testing on RPi

---
+++ windowTest/src/joystickArduino/TestForHardware.java
@@ -0,0 +1,18 @@
+package joystickArduino;
+
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+
+public class TestForHardware {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Controller[] con = ControllerEnvironment.getDefaultEnvironment().getControllers();
+		System.out.println("    List of Available Controllers: ");
+		for(int i = 0; i < con.length; i++){
+			System.out.println(i + "  " + con[i].getName());
+		}
+		System.out.println("    (End of Program)");
+	}
+
+}
