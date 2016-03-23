5ddd081bef5c9f12afac581274acb9a9be3a935f
superrm11 <ramtech51@comcast.net>
2016-03-23 17:20:33 -0400

    to help merge code

--- windowTest/src/joystickArduino/TestForHardware.java
+++ windowTest/src/joystickArduino/TestForHardware.java
@@ -4,9 +4,11 @@ import net.java.games.input.Controller;
 import net.java.games.input.ControllerEnvironment;
 
 public class TestForHardware {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.loadLibrary("jinput-linux");
 		Controller[] con = ControllerEnvironment.getDefaultEnvironment().getControllers();
 		System.out.println("    List of Available Controllers: ");
 		for(int i = 0; i < con.length; i++){
