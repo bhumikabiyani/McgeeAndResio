7179302b07eab6bcd249d77eb6bcf4823727aa34
superrm11 <ramtech51@comcast.net>
2016-03-29 20:12:36 -0400

    This will be just a reference point from now on.

--- windowTest/src/joystickArduino/FrameEdit.java
+++ windowTest/src/joystickArduino/FrameEdit.java
@@ -22,12 +22,7 @@ import java.awt.Color;
 import javax.swing.JList;
 
 public class FrameEdit extends JFrame {
-	private static Controller[] con;
-	private static JMenu mnDevice;
-	private static JMenu mnAdd;
-	private static Component[][] com;
-	private static JRadioButtonMenuItem showAllControllers;
-	private JPanel contentPane;
+	
 
 	/**
 	 * Launch the application.
@@ -53,123 +48,6 @@ public class FrameEdit extends JFrame {
 	 */
 	
 public FrameEdit() {
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JMenu mnFile = new JMenu("File");
-		menuBar.add(mnFile);
-		
-		JMenuItem mntmSave = new JMenuItem("Save");
-		mnFile.add(mntmSave);
-		
-		JMenuItem mntmSaveAs = new JMenuItem("Save As");
-		mnFile.add(mntmSaveAs);
-		
-		JMenuItem mntmOpen = new JMenuItem("Open");
-		mnFile.add(mntmOpen);
-		
-		JMenuItem mntmExit = new JMenuItem("Exit");
-		mnFile.add(mntmExit);
-		mntmExit.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				System.exit(1);
-				
-			}
-			
-		});
-		
-		
-		mnAdd = new JMenu("Add");
-		menuBar.add(mnAdd);
-		
-		mnDevice = new JMenu("Device");
-		
-		showAllControllers = new JRadioButtonMenuItem("Show All Controllers");
-		showAllControllers.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				mnAdd.doClick();
-				if(showAllControllers.isSelected()){
-					mnDevice.removeAll();
-					for(int i = 0; i < con.length; i++){
-						mnDevice.add(con[i].getName());
-					}
-				}else {
-					mnDevice.removeAll();
-					for(int i = 0; i < con.length; i++){
-						if(con[i].getType() == Controller.Type.GAMEPAD || con[i].getType() == Controller.Type.KEYBOARD){
-							mnDevice.add(con[i].getName());
-						}
-						
-					}
-				}
-			}
-			
-		});
-		
-		mnAdd.add(mnDevice);
-		
-		mnAdd.add(showAllControllers);
-		
-		JMenu mnView = new JMenu("View");
-		menuBar.add(mnView);
-		
-		JMenuItem mntmMaximize = new JMenuItem("Maximize");
-		mnView.add(mntmMaximize);
-		mntmMaximize.addActionListener(new ActionListener(){
-
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-			
-		});
-		
-		JMenu mnSetup = new JMenu("Setup");
-		menuBar.add(mnSetup);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.GRAY);
-		panel.setBounds(0, 0, 139, 88);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		panel.setVisible(false);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(0, 0, 139, 22);
-		panel.add(comboBox);
-		
-		JButton btnAddAsButton = new JButton("Add As Button");
-		btnAddAsButton.setBounds(0, 25, 127, 25);
-		panel.add(btnAddAsButton);
-		
-		JButton btnAddAsAxis = new JButton("Add As Axis");
-		btnAddAsAxis.setBounds(0, 50, 127, 25);
-		panel.add(btnAddAsAxis);
-		
-		JPanel componentPanel = new JPanel();
-		componentPanel.setBackground(Color.LIGHT_GRAY);
-		componentPanel.setBounds(173, 0, 259, 227);
-		contentPane.add(componentPanel);
-		componentPanel.setLayout(null);
-		
-		JList buttonList = new JList();
-		buttonList.setBounds(12, 13, 116, 201);
-		componentPanel.add(buttonList);
-		
-		JList axisList = new JList();
-		axisList.setBounds(129, 13, 118, 201);
-		componentPanel.add(axisList);
 		
 		
 	}
