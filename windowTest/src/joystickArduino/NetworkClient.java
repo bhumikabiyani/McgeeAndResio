c19c099e1d9bfa7b238e1e2e21a31f326958bc8f
Ryan <ramtech51@comcast.net>
2016-05-12 22:57:41 -0400

    Tried to get sending byte[] over socket, failed.

--- windowTest/src/joystickArduino/NetworkClient.java
+++ windowTest/src/joystickArduino/NetworkClient.java
@@ -3,16 +3,21 @@ package joystickArduino;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import org.omg.CORBA.portable.InputStream;
+
 public class NetworkClient {
 
-	public static void main(String[] args) throws  IOException {
+	public static void main(String[] args) throws  IOException, ClassNotFoundException {
 		String serverAdress = "localhost";
 		Socket socket = new Socket(serverAdress, 9090);
-		BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		System.out.println(input.readLine());
+		ObjectInputStream ois;
+		ois = new ObjectInputStream(socket.getInputStream());
+		byte[] temp = (byte[]) ois.readObject();
+		System.out.println(temp[0]);
 	}
 
 }
